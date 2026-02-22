43e235a03978653e64dc1f2ff12c1f12bc8283b6
tate.b <121896551+tateb14@users.noreply.github.com>
2026-02-22 13:19:06 -0600

    Removed guildMember intent

--- bot/bot.js
+++ bot/bot.js
@@ -5,7 +5,7 @@ const wipeShifts = require("./Functions/wipeShifts")
 const mongoose = require('mongoose')
 const handlers = fs.readdirSync('bot/Handlers').filter(file => file.endsWith('.js'));
 const client = new Client({
-    intents: [IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.Guilds],
+    intents: [IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.Guilds],
 })
 for (const handler of handlers) {
     require(`./Handlers/${handler}`)(client);
